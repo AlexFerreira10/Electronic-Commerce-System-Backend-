6c3a309c8fac9d007c6cdb31b050804c41c346b4
AlexFerreira10 <correaferreiraalex@gmail.com>
2024-02-28 16:59:45 -0300

    Implemented user insertion, deletion and update

--- src/main/java/demo/resource/UserResource.java
+++ src/main/java/demo/resource/UserResource.java
@@ -1,13 +1,19 @@
 package demo.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import demo.model.entities.User;
 import demo.model.service.UserService;
@@ -17,6 +23,8 @@ import demo.model.service.UserService;
 //EndPoint = is a specific URL that represents a resource or functionality of a system.
 @RequestMapping(value = "/users") 
 public class UserResource {
+	
+	//GET EndPoint = Data recovery in database
 	
 	@Autowired
 	UserService service;
@@ -35,4 +43,31 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	//Post = http method for insert
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		//Inseri recurso e mais adequado retornar o codigo 201; siginifca que adicionou um recurso
+		//Quando retorna o 201 é necessárop colocar um cabeçalho location URI
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
